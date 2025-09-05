261baa5b2791ed24a3ebe822803ed252fff8bb29
Dwi Permana Putra <dwi.putra@icloud.com>
2025-09-05 17:22:24 +0800

    init recalculateKunjungan.js

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,10 @@ import admin from "firebase-admin";
 import { incrementBumilCount } from "./modules/incrementBumil.js";
 import { decrementBumilCount } from "./modules/decrementBumil.js";
 import { recalculateBumilStats } from "./modules/recalculateBumil.js";
+import { recalculateKunjunganStats } from "./modules/recalculateKunjungan.js";
 
 if (!admin.apps.length) {
   admin.initializeApp();
 }
 
-export { incrementBumilCount, decrementBumilCount, recalculateBumilStats };
+export { incrementBumilCount, decrementBumilCount, recalculateBumilStats, recalculateKunjunganStats };
